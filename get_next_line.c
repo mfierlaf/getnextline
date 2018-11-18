9a401f2ecd889ba22a3331ef89d082f3e4430a3f
mfierlaf <43782114+mfierlaf@users.noreply.github.com>
2018-11-18 15:58:27 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mfierlaf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 15:15:07 by mfierlaf          #+#    #+#             */
-/*   Updated: 2018/11/16 14:38:37 by mfierlaf         ###   ########.fr       */
+/*   Updated: 2018/11/18 15:57:31 by mfierlaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,16 @@
 int	get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	char		*tmp;
-	int			j;
 	int			i;
 	int			k;
 	int 		ret;
 	static char	*stock;
 	char		*s[2];
 
-	j = 0;
 	k = 0;
 	ret = 0;
 	i = 0;
-	if ((tmp = ft_strnew(0)) == NULL)
+	if ((*line = ft_strnew(0)) == NULL)
 		return (-1);
 	if (stock != NULL)
 	{
@@ -36,8 +33,8 @@ int	get_next_line(const int fd, char **line)
 			i++;
 		if (stock[i] == '\0')
 		{
-			s[0] = tmp;
-			tmp = ft_strjoin(s[0], stock);
+			s[0] = *line;
+			*line = ft_strjoin(s[0], stock);
 			free(s[0]);
 			free(stock);
 		}
@@ -46,13 +43,10 @@ int	get_next_line(const int fd, char **line)
 			if ((s[0] = ft_strnew(i)) == NULL)
 				return (-1);
 			ft_strncpy(s[0], stock, i);
-			s[1] = tmp;
-			tmp = ft_strjoin(s[1], s[0]);
+			s[1] = *line;
+			*line = ft_strjoin(s[1], s[0]);
 			free(s[0]);
 			free(s[1]);
-			if ((*line = ft_strnew(ft_strlen(tmp))) == NULL)
-				return (-1);
-			*line = ft_strcpy(*line, tmp);
 			s[1] = stock;
 			stock = ft_strsub(stock, i + 1, (ft_strlen(stock) - (i + 1)));
 			free(s[1]);
@@ -61,31 +55,36 @@ int	get_next_line(const int fd, char **line)
 	}
 	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
 	{
+		printf ("ret = %d\n", ret);
 		i = 0;
 		buf[ret] = '\0';
-		while (buf[i] != '\n' && buf[i] != '\0')
+		printf("buf = %s\n", buf);
+		while (buf[i] != '\n' && i < ret)
 		{
 			i++;
 		}
+		printf("i = %d\n", i);
 		if (i == ret)
 		{
-			s[0] = tmp;
-			tmp = ft_strjoin(tmp, buf);
-			free(s[0]);
+			printf("test");
+			s[1] = *line;
+			printf("s1 = %s\n", s[1]);
+			*line = ft_strjoin(s[1], buf);
+			printf("boucle = %s\n", *line);
+			free(s[1]);
 		}
 		else
 		{
-			k = 0;
+			printf("alors\n");
 			buf[i] = '\0';
-			s[0] = tmp;
-			tmp = ft_strjoin(tmp, buf);
+			printf("check 0 = %s\n", buf);
+			s[0] = *line;
+			printf("s0 = %s\n", s[0]);
+			*line = ft_strjoin(s[0], buf);
+			printf("line 2 = %s\n", *line);
 			free(s[0]);
-			if ((*line = ft_strnew(ft_strlen(tmp))) == NULL)
-				return (-1);
-			*line = ft_strcpy(*line, tmp);
-			free(tmp);
 			if ((stock = ft_strnew(BUFF_SIZE)) == NULL)
-			return (-1);
+				return (-1);
 			i++;
 			while (i < ret)
 			{
@@ -94,14 +93,9 @@ int	get_next_line(const int fd, char **line)
 				i++;
 			}
 			stock[k] = '\0';
+		//	printf("stock = %s\n", stock);
 			return (1);
 		}
-	}
-	if (tmp != NULL)
-	{
-		if ((*line = ft_strnew(ft_strlen(tmp))) == NULL)
-			return (-1);
-		*line = ft_strjoin(*line, tmp);
 	}
 	return (0);
 }
