d7a83a0835bb8013681a935bbf251f0f51a0c34f
mfierlaf <43782114+mfierlaf@users.noreply.github.com>
2018-11-22 14:47:30 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mfierlaf <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/06 15:15:07 by mfierlaf          #+#    #+#             */
-/*   Updated: 2018/11/19 09:36:54 by mfierlaf         ###   ########.fr       */
+/*   Updated: 2018/11/22 10:29:39 by mfierlaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,73 +14,74 @@
 #include "stdio.h"
 #include <limits.h>
 
-char *ft_stock(char **line, char *stock)
+int		join(char **src, char *add)
 {
-	int i;
-	char *s;
+	char *tmp;
+
+	tmp = *src;
+	if ((*src = ft_strjoin(tmp, add)) == NULL)
+		return (-1);
+	ft_strdel(&tmp);
+	return (1);
+}
+
+char	*ft_stock(char **line, char *stock)
+{
+	int		i;
+	char	*s;
 
 	i = 0;
 	while (stock[i] != '\n' && stock[i] != '\0')
 		i++;
 	if (stock[i] == '\0')
 	{
-		s = *line;
-		*line = ft_strjoin(s, stock);
-		ft_strdel(&s);
+		if (join(*&line, stock) == -1)
+			return (NULL);
 		ft_strdel(&stock);
 	}
 	else
 	{
 		if ((s = ft_strnew(i)) == NULL)
-			return (NULL);
-		ft_strncpy(s, stock, i);
-		*line = ft_strdup(s);
+			return (ERROR);
+		ft_strncpy(s, stock, i++);
+		join(*&line, s);
 		ft_strdel(&s);
 		s = stock;
-		i++;
 		stock = ft_strsub(s, i, (ft_strlen(stock) - (i)));
 		ft_strdel(&s);
 		return (stock);
 	}
-	return(NULL);
+	return (NULL);
 }
 
 char	*endofl(char *buf, char **line, int ret, char *stock)
 {
-	int k;
-	int i;
-	char *s;
+	int		k;
+	int		i;
 
 	i = 0;
 	while (buf[i] != '\n' && i < ret)
 		i++;
 	k = 0;
 	buf[i] = '\0';
-	s = *line;
-	*line = ft_strjoin(*line, buf);
-	ft_strdel(&s);
+	join(*&line, buf);
 	if ((stock = ft_strnew(BUFF_SIZE)) == NULL)
-		return (NULL);
+		return (ERROR);
 	i++;
 	while (i < ret)
 	{
-		stock[k] = buf[i];
-		k++;
-		i++;
+		stock[k++] = buf[i++];
 	}
 	stock[k] = '\0';
 	return (stock);
 }
 
-char *boucle(char **line, char *buf, char *stock, int fd)
+char	*boucle(char **line, char *buf, char *stock, int fd)
 {
-	char *s;
-	int i;
-	int ret;
+	int		i;
+	int		ret;
 
-	i = 0;
 	ret = 0;
-
 	while ((ret = (read(fd, buf, BUFF_SIZE))) > 0)
 	{
 		i = 0;
@@ -89,32 +90,39 @@ char *boucle(char **line, char *buf, char *stock, int fd)
 			i++;
 		if (i == ret)
 		{
-			s = *line;
-			*line = ft_strjoin(s, buf);
-			ft_strdel(&s);
+			join(*&line, buf);
 			if (ret < BUFF_SIZE)
 				return (*line);
 		}
 		else
-			if ((stock = endofl(buf, line, ret, stock)) != NULL)
+		{
+			if ((stock = endofl(buf, line, ret, &stock[fd])) == ERROR)
+				return (ERROR);
+			else if (stock != NULL)
 				return (stock);
+		}
 	}
 	return (NULL);
 }
 
-
-int	get_next_line(const int fd, char **line)
+int		get_next_line(const int fd, char **line)
 {
 	char		buf[BUFF_SIZE + 1];
-	static char *stock;
+	static char *stock[OPEN_MAX];
 
 	if ((*line = ft_strnew(0)) == NULL)
 		return (-1);
-	if (stock != NULL)
-		if ((stock = ft_stock(line, stock)) != NULL)
+	if (stock[fd] != NULL)
+	{
+		if ((stock[fd] = ft_stock(line, stock[fd])) == ERROR)
+			return (-1);
+		else if (stock[fd] != NULL)
 			return (1);
-	if ((stock = boucle(line, buf, stock, fd)) != NULL)
+	}
+	if ((stock[fd] = boucle(line, buf, stock[fd], fd)) == ERROR)
+		return (-1);
+	else if (stock[fd] != NULL)
 		return (1);
-	ft_strdel(&stock);
+	ft_strdel(&stock[fd]);
 	return (0);
 }
